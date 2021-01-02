22c33f78ebd2a9f64dd16bb780097d05b041b044
Liudmila Budanitskaya <liudmila28march1991@gmail.com>
2021-01-02 16:44:11 +0300

    Hey, I just completed Kotlin Koans. https://kotlinlang.org/docs/tutorials/koans.html #kotlinkoans

--- Generics/Generic functions/src/Task.kt
+++ Generics/Generic functions/src/Task.kt
@@ -1,10 +1,9 @@
 import java.util.*
+import kotlin.collections.ArrayList
 
-/*fun <T, C : MutableCollection<in T>> Array<out T>.partitionTo(// где тут условие?
-    destination: C
-): C {
-    this.partition { it. }
-    return C()
+fun <T, C : MutableCollection<in T>> Collection<T>.partitionTo(destination1: C, destination2: C, predicate: (T) -> Boolean): Pair<C, C> {
+    val(one, two) = this.partition(predicate)
+    return Pair(one.toCollection(destination1), two.toCollection(destination2))
 }
 
 fun partitionWordsAndLines() {
@@ -14,15 +13,10 @@ fun partitionWordsAndLines() {
     check(lines == listOf("a b", "d e"))
 }
 
+
 fun partitionLettersAndOtherSymbols() {
     val (letters, other) = setOf('a', '%', 'r', '}')
         .partitionTo(HashSet(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
     check(letters == setOf('a', 'r'))
     check(other == setOf('%', '}'))
-}*/
-
-fun main() {
-/*    partitionLettersAndOtherSymbols()
-    partitionWordsAndLines()*/
-    val arrayList = intArrayOf(1, 2, 5).toCollection(ArrayList())
 }
